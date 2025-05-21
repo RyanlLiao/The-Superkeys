f46f213650b4b503930815d4e275e5df9241b74b
Domy05 <u24580482@tuks.co.za>
2025-05-21 10:27:39 +0200

    dashboard and header stuffies

--- header.php
+++ header.php
@@ -1,42 +1,11 @@
+<link rel="stylesheet" href="css/header.css">
 
 <nav class="navbar">
     <ul>
         <li><a href="signup.php">Sign Up</a></li>
-        <li><a href="login.php">Login</a></li>
-              
+        <li><a href="login.php">Login</a></li>        
     </ul>
 </nav>
 
 <!-- <hr> -->
 
-<style>
-    .navbar {
-    background-color: #0B1957;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 20px;
-    flex-wrap: wrap;
-}
-  
-.navbar ul {
-    list-style-type: none;
-    display: flex;
-    gap: 10px;
-    padding: 0;
-    margin: 0;
-}
-  
-.navbar li a {
-    color: white;
-    text-decoration: none;
-    padding: 10px 15px;
-    border: 1px solid white;
-    border-radius: 5px;
-    display: block;
-}
-  
-.navbar li a:hover {
-    background-color: black;
-}
-</style>
