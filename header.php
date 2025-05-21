4c9d93f12b7f334963167af8005a929227a7f631
babychucks <babychucks05@gmail.com>
2025-05-21 02:45:39 +0200

    Added products page and img folder

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <nav class="navbar">
     <ul>
         <li><a href="signup.php">Sign Up</a></li>
-        <li><a href="login.php">Login</a></li>        
+        <li><a href="login.php">Login</a></li>
+              
     </ul>
 </nav>
 
