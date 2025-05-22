c0d91132fd49952e235ac114465fb43423b012e0
Domy05 <u24580482@tuks.co.za>
2025-05-22 09:47:22 +0200

    other dashboard changes

--- header.php
+++ header.php
@@ -3,7 +3,11 @@
 <nav class="navbar">
     <ul>
         <li><a href="signup.php">Sign Up</a></li>
-        <li><a href="login.php">Login</a></li>        
+        <li><a href="login.php">Login</a></li>  
+        <li><a href="wishlist.php">Wishlist</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="dashboard.php">Manager Dashboard</a></li>   
+        <li><a href="customerDashboard.php">Customer Dashboard</a></li>   
     </ul>
 </nav>
 
