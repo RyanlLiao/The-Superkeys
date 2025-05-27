2c981f6bc50fa471b9912ae0639d258eacb4999f
Domy05 <u24580482@tuks.co.za>
2025-05-27 09:59:47 +0200

    graph manager js count code

--- js/graphManager.js
+++ js/graphManager.js
@@ -1,22 +1,48 @@
-src="https://cdn.jsdelivr.net/npm/chart.js"
+var a_key = 'f61891d740ceb2539535bd06a1c7936f';
 
-const ctx = document.getElementById('myChart');
+function fetchAndGraphRatings() {
+    var requestBody = {
+        type: "GetAllProducts",
+        apikey: a_key,
+        return: "*"
+    };
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['1 Star', '2 Stars', '3 Stars', '4 Stars', '5 Stars'],
-      datasets: [{
-        label: 'Number of products per rating',
-        data: [19, 30, 5, 2, 3],        //VALUES CAN BE CHANGED FROM HERE
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost/The-Superkeys/backend/api.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.status === "success") {
+                var data = response.data;
+                var ratingCounts = countRatings(data);
+                console.log("Rating counts:", ratingCounts);
+                renderChart(ratingCounts);
+            } else {
+                console.error("API returned error:", response.message);
+            }
         }
-      }
+    };
+
+    xhr.send(JSON.stringify(requestBody));
+}
+
+function countRatings(products) {
+    var ratingCounts = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    };
+
+    for (var i = 0; i < products.length; i++) {
+        var rating = Math.round(parseFloat(products[i].average_rating));
+        if (rating >= 1 && rating <= 5) {
+            ratingCounts[rating]++;
+        }
     }
-  });
+
+    return ratingCounts;
+}
